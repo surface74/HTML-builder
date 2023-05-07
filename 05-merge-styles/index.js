d963ed5eacc2451ef97ad9d339d61f0253a04c42
surface74 <warwhisper@tut.by>
2023-05-07 18:42:40 +0300

    fix(task #5): correct async write to file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,7 @@
 const { createWriteStream, createReadStream } = require('fs');
 const { join } = require('path');
 const { readdir } = require('fs/promises');
+const { pipeline } = require('stream');
 
 const destFile = join(process.argv[1], 'project-dist', 'bundle.css');
 const sourcePath = join(process.argv[1], 'styles');
@@ -8,17 +9,21 @@ const sourcePath = join(process.argv[1], 'styles');
 mergeFiles(sourcePath, destFile, 'css');
 
 async function mergeFiles(sourcePath, destinationFile, fileExtention) {
-  let output = createWriteStream(destinationFile);
-  await readdir(sourcePath, { withFileTypes: true })
-    .then(entries => entries
-      .filter(entry => entry.isFile() && entry.name.match(new RegExp(`.${fileExtention}$`, 'i')))
-      .forEach((entry, index) => {
-        if (index) {
-          output = createWriteStream(destinationFile, { flags: 'a+' });
-        }
-        const input = createReadStream(join(sourcePath, entry.name), 'utf-8');
-        input.pipe(output);
-      }))
-    .catch(err => console.error(err));
+  try {
+    const dirEntries = await readdir(sourcePath, { withFileTypes: true });
+    const files = dirEntries
+      .filter(entry => entry.isFile() && entry.name.match(new RegExp(`.${fileExtention}$`, 'i')));
+
+    createWriteStream(destinationFile).close(); // clear destination file
+
+    for (const file of files) {
+      pipeline(
+        createReadStream(join(sourcePath, file.name), 'utf-8'),
+        createWriteStream(destinationFile, { flags: 'a+' }),
+        (error) => { if (error) { console.log(error); } });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
